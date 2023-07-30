16fa6217777c6b35b7e77a8c56277807327a138e
Akram-w <akramwaseem792@gmail.com>
2023-07-30 11:26:34 +0530

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
 import { StoreModule } from '@ngrx/store'
 import { booksReducer } from './state/books.reducer';
 import { collectionReducer } from './state/collection.reducer';
